172e4fdc850e54053d1d588b53f4fdd355de8aaf
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-11 21:12:30 +0000

    Add task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -67,4 +67,44 @@ $(function () {
 
     $('.amenities h4').text(toDisplay);
   });
+
+  $('button').click(function () {
+    const selectedAmenities = {};
+    selectedAmenities.amenities = [];
+
+    $('input[type="checkbox"]:checked').each(function () {
+      selectedAmenities.amenities.push($(this).data('id'));
+    });
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify(selectedAmenities),
+      success: function (data) {
+      // Clear existing places
+        $('.places').empty();
+
+        // Add filtered places
+        data.forEach(function (place) {
+          $('.places').append(`
+            <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>
+          `);
+        });
+      }
+    });
+  });
 });
